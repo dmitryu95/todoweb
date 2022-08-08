4ce7b49a78ed47df357dac95566389c0877d17fc
dmitryu95 <dmitriyu1428@gmail.com>
2022-08-08 13:15:52 +0700

    add gitIgnore and fix some files

--- src/source/stores/NoteStore.js
+++ src/source/stores/NoteStore.js
@@ -1,85 +1,83 @@
 import { Network } from "../components/Network";
-import {action, makeObservable, observable} from 'mobx';
+import { action, makeObservable, observable } from "mobx";
 import { store } from "./AuthStore";
 
 class NoteStore {
-    constructor() {
-        this.title = '';
-        this.listOfItems = ["TEST"];
-        this.statusItem = false;
-        
-            makeObservable(this, {
-                title: observable,
-                statusItem: observable,
-                listOfItems: observable,
-                addNewNote: action.bound,
-                setTitle: action.bound, 
-                setListOfItems: action.bound,
-                changeStatus: action.bound,
-                editItem: action.bound,
-                delete: action.bound,
-            })
-    }
- 
-    setListOfItems() {
-        Network('tasks?access_token=', store.token, 'GET')
-        .then(result => {
-            this.listOfItems = result
-            console.log("проверка", this.listOfItems.map(item => item.title));
-          })
-    }
+  constructor() {
+    this.title = "";
+    this.listOfItems = ["TEST"];
+    this.statusItem = false;
 
-    setTitle(text) {
-        this.title = text.target.value; 
-    }
+    makeObservable(this, {
+      title: observable,
+      statusItem: observable,
+      listOfItems: observable,
+      addNewNote: action.bound,
+      setTitle: action.bound,
+      setListOfItems: action.bound,
+      changeStatus: action.bound,
+      editItem: action.bound,
+      delete: action.bound,
+    });
+  }
 
-    addNewNote() {
-        console.log("содержание в поле title", this.title);
-    
-        if(!this.title || this.title == '' || this.title == ' ') {
-            // this.setListOfItems()
-            alert("Поле с заметкой пустое")
-        } else {
-            Network('tasks?access_token=', store.token, 'POST', {title: this.title});
-            setTimeout(this.setListOfItems, 200);
-            this.title = ''
-        }  
-    }
+  async setListOfItems() {
+    this.listOfItems = await Network("tasks?access_token=", store.token, "GET");
+    console.log(
+      "проверка",
+      this.listOfItems.map((item) => item.title)
+    );
+  }
 
-    changeStatus(item) {
-        if(item.done == true) {
-            Network('tasks?access_token=', store.token, 'PUT', {
-                title: item.title,
-                id: item.id,
-                done: false
-            })
-        } else {
-            Network('tasks?access_token=', store.token, 'PUT', {
-                title: item.title,
-                id: item.id,
-                done: true
-            })
-        }
-        setTimeout(this.setListOfItems, 200);
-        this.setListOfItems();
-    }
+  setTitle(text) {
+    this.title = text.target.value;
+  }
 
-    editItem(item) {
-        this.title = prompt("Edit Note", item.title)
-        Network('tasks?access_token=', store.token, 'PUT', {
-            id: item.id,
-            done: item.done,
-            title: this.title})
-        setTimeout(this.setListOfItems, 200);
-        this.setListOfItems();
-        this.title = ''
+  async addNewNote() {
+    if (!this.title || this.title == "" || this.title == " ") {
+      alert("Поле с заметкой пустое");
+    } else {
+      await Network("tasks?access_token=", store.token, "POST", {
+        title: this.title,
+      });
+	  await this.setListOfItems();
+      this.title = "";
     }
+  }
 
-    delete(id) {
-        Network(`tasks/${id}?access_token=`, store.token, 'DELETE');
-		setTimeout(this.setListOfItems, 200);
-        this.setListOfItems()
+  async changeStatus(item) {
+    if (item.done == true) {
+      await Network("tasks?access_token=", store.token, "PUT", {
+        title: item.title,
+        id: item.id,
+        done: false,
+      });
+    } else {
+      await Network("tasks?access_token=", store.token, "PUT", {
+        title: item.title,
+        id: item.id,
+        done: true,
+      });
     }
+    await this.setListOfItems();
+  }
+
+  editItem(item) {
+    this.title = prompt("Edit Note", item.title);
+    Network("tasks?access_token=", store.token, "PUT", {
+      id: item.id,
+      done: item.done,
+      title: this.title,
+    });
+    setTimeout(this.setListOfItems, 200);
+    this.setListOfItems();
+    this.title = "";
+  }
+
+  async delete(id) {
+    await Network(`tasks/${id}?access_token=`, store.token, "DELETE");
+    await this.setListOfItems();
+  }
 }
 
 export const StoreTodo = new NoteStore();
