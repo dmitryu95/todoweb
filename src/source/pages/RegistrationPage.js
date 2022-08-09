08453dea86d341ed18af9a789e8ba4357024dc99
dmitryu95 <dmitriyu1428@gmail.com>
2022-08-09 15:25:48 +0700

    fix all componets

--- src/source/pages/RegistrationPage.js
+++ src/source/pages/RegistrationPage.js
@@ -7,50 +7,53 @@ import { store } from "../stores/AuthStore";
 const Registration = observer(() => {
 	const navigate = useNavigate();
 
+	const onClick = async () => {
+		const page = await store.sendAuthDate(
+			"Users", 
+			"POST", 
+			"registration")
+			navigate(page)
+	}
 	return (
 		<form>
-		<div className="RegistrationPage">
-			<div className="RegistrationHeader">
-			<p>Регистрация</p>
+			<div className="RegistrationPage">
+				<div className="RegistrationHeader">
+					<p>Регистрация</p>
+				</div>
+				<div className="MainWindowRegistration">
+					<div>
+						<p>Enter your Email:</p>
+						<input
+							className="Input"
+							type="text"
+							value={store.email}
+							onChange={store.setEmail}
+							placeholder="Enter email..."
+						/>
+					</div>
+					<div>
+						<p>Enter your Password:</p>
+						<input
+							className="Input"
+							type="text"
+							value={store.password}
+							onChange={store.setPassword}
+							placeholder="Enter password..."
+						/>
+					</div>
+					<button
+						type="button"
+						onClick={onClick}
+						>
+						Accept
+					</button>
+					<Link to="/">
+						<button type="button">Back to login</button>
+					</Link>
+				</div>
 			</div>
-			<div className="MainWindowRegistration">
-			<div>
-				<p>Enter your Email:</p>
-				<input
-				className="Input"
-				type="text"
-				value={store.email}
-				onChange={store.setEmail}
-				placeholder="Enter email..."
-				/>
-			</div>
-			<div>
-				<p>Enter your Password:</p>
-				<input
-				className="Input"
-				type="text"
-				value={store.password}
-				onChange={store.setPassword}
-				placeholder="Enter password..."
-				/>
-			</div>
-			<button
-				type="button"
-				onClick={ async () => {
-					const authResult = await store.sendAuthDate("Users", "POST", "registration")
-					if (authResult) navigate("/ListPage");
-            		else navigate("/errorPage");}
-				}>
-				Accept
-			</button>
-			
-			<Link to="/">
-				<button type="button">Back to login</button>
-			</Link>
-			</div>
-		</div>
 		</form>
-	);
+	)
 	});
 
 export { Registration };
